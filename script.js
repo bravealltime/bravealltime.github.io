ea16d7f8cb0a95e0ea3909570b85e9ce4ac49be1
bravealltime <bravealltime1997@gmail.com>
2025-06-21 22:21:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3216,6 +3216,7 @@ document.addEventListener('DOMContentLoaded', function() {
             img.src = e.target.result;
             img.className = 'max-h-48 rounded-lg object-contain mx-auto';
             previewContainer.appendChild(img);
+            previewContainer.classList.remove('hidden');
         };
         reader.readAsDataURL(file);
     }
